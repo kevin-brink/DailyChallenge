74f171b08008322b32cf11beb5a1621468e098a9
Kevin Brink <Kevin.Brink@wolterskluwer.com>
2023-05-24 14:19:25 -0500

    Finished 11

---
+++ Daily_0011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Daily_0011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine(string.Join(',', Autocomplete("de", new() { "dog", "deal", "deer" })));
+        }
+
+        public static IEnumerable<string> Autocomplete(string partialWord, List<string> wordList)
+        {
+            return wordList.Where(x => x.StartsWith(partialWord));
+        }
+    }
+}
