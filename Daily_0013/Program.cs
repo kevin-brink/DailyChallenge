b1d5072123d29a931b85f5a3077b4c89c717258b
Kevin Brink <Kevin.Brink@wolterskluwer.com>
2023-05-30 14:52:19 -0500

    Finished 13

---
+++ Daily_0013/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Daily_0013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string given = "hello person....how are you today? hahahaha";
+            var result = DistinctSubstring(given, 2);
+            Console.WriteLine($"Longest substring of '{given}' is '{result}'.");
+        }
+
+        static string DistinctSubstring(string given, int k)
+        {
+            var substring = "";
+
+            for (int i = 0; i < given.Length; i++)
+            {
+                for (int j = i; j < given.Length; j++)
+                {
+                    IEnumerable<char> distinctCharacters = given[i..j].Distinct();
+                    if (distinctCharacters.Count() > k)
+                        break;
+
+                    if (given[i..j].Length > substring.Length)
+                            substring = given[i..j];
+                }
+            }
+
+            return substring;
+        }
+    }
+}
